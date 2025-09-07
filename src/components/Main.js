71d5a13ad4434876ab889629829098510362a35d
ziyamohammad <mohammad2311061@akgec.ac.in>
2025-09-07 18:52:33 +0530

    some css changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,12 +4,13 @@ import { Link } from 'react-router'
 
 const Main = () => {
   return (
+    <>
     <div className={styles.main}>
       <div className={styles.authority}>
          <span className={styles.head}>
             For Authority
          </span>
-         <span className={styles.content}>Monitor hazards faster, verify citizen alerts, and respond with confidence.</span>
+         <span className={styles.content}>Stay informed with real-time hazard reports from citizens. With verified alerts and faster communication, authorities can coordinate responses effectively and ensure the safety of coastal communities.</span>
          <button className={styles.login}>Login</button>
          <button className={styles.report}>Report</button>
          <span className={styles.join}>Don't have an account?</span>
@@ -20,13 +21,17 @@ const Main = () => {
         <span className={styles.head1}>
             For Citizens
          </span>
-         <span  className={styles.content}>Your eyes on the coast can protect thousands — share, receive, and act together.</span>
+         <span  className={styles.content}>Your eyes on the coast can make a real difference. By reporting hazards instantly and sharing vital information, you help protect lives and support safer shores for everyone.</span>
          <button className={styles.login}>Login</button>
          <button className={styles.report}>Report</button>
          <span className={styles.join}>Don't have an account?</span>
          <Link to ="/citisignup" className={styles.link}>Sign Up</Link>
       </div>
     </div>
+    <div className={styles.dashboard}>
+      
+    </div>
+    </>
   )
 }
 
