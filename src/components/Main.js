851ebc7aebb037d49af2f160f71a72d9d761d61f
arpitgithub07072005 <arpitpandey07072005@gmail.com>
2025-09-17 18:50:13 +0530

    set conflict

--- src/components/Main.js
+++ src/components/Main.js
@@ -79,6 +79,61 @@ const handlesubmit = async(e)=> {
       })}
       </div>
     </div>
+    <div className={style.container} >
+          <div className={style.heading}>About Us</div>
+          <div className={style.boxes}>
+            <div className={style.left}>
+              <div className={style.head}>About Samudram</div>
+              <p className={style.para}>Harnessing the power of citizens, AI, and ocean intelligence to build safer coastlines.</p>
+              <p className={style.dis}>Samudram is an ocean hazard monitoring platform built to connect citizens and authorities in real time. We combine citizen reports, satellite & social media data, and verified alerts from agencies to create a trusted disaster dashboard. Our mission is simple yet powerful: empower every citizen to be a guardian of the sea, and every authority to act with precision and confidence</p>
+              <div className={style.img3}>
+                <img src="/about1.png" alt="" />
+              </div>
+            </div>
+            <div className={style.right}>
+              <div className={style.img}>
+                <img src="/about2.png" alt=""   />
+              </div>
+              <div className={style.imgcont}>
+                <div className={style.img1}>
+                <img src="/about3.png" alt=""  />
+                </div>
+                <div className={style.img2}>
+                <img src="/about4.png" alt=""  />
+                </div>
+              </div>
+               <div className={style.head}>Our Mission & Our Vision</div>
+              <p className={style.dis}>To empower citizens and authorities with real-time ocean intelligence, enabling faster reporting, smarter decisions, and safer coastlines. Harnessing the power of citizens, AI, and data to build a trusted network that detects, verifies, and responds to coastal hazards. To connect communities and agencies through timely alerts, citizen reports, and verified data for effective disaster response.</p>
+              <p className={style.dis}>A future where every coastline is resilient, every citizen is informed, and every authority acts with precision. To become the world’s most trusted citizen-driven platform for coastal safety and ocean hazard intelligence. An empowered global community where technology, science, and citizens work together to protect lives and the sea.</p>
+            </div>
+          </div>
+        </div>
+        <div className={styl.container}>
+          <div className={style.heading}>Contact Us</div>
+          
+          <div className={styl.cont}>
+            <div className={styl.left}>
+            <div className={styl.head1}>Send a message</div>
+            <div className={styl.nameinput}>
+            <input type="text" className={styl.namefirst} placeholder='First Name'/><input type="text" className={styl.namesecond} placeholder='Last Name'/>
+            </div>
+<input type="email" placeholder='Your Email' />
+<input type="text" value={"+91"} className={styl.fixed}/><input type="number"  placeholder='Phone number' className={styl.number}/>
+<textarea name="" id="" rows={4} className={styl.textarea} placeholder='How can we help?'></textarea>
+<button className={styl.submit}>Submit</button>
+            </div>
+            <div className={styl.right}>
+              <div className={styl.head}>Get in Touch</div>
+              <div className={styl.location}>📍 Location</div>
+              <p className={styl.locat}>Marine Drive, Mumbai, India</p>
+              <div className={styl.location}>📧 Email</div>
+              <p className={styl.locat}>support@samudram.org</p>
+              <div className={styl.location}>📞 Phone</div>
+              <p className={styl.locat}>+91 98765 43210</p>
+            </div>
+          </div>
+
+        </div>
 
     {model && (
       <div className={styles.modal}>
